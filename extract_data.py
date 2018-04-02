f47c9333354de7684def8418b2c56a164f449c74
Nidhi <nidhi0613@gmail.com>
2018-04-02 18:12:25 -0400

    Bug fix in ProcessBank extraction script

--- extract_data.py
+++ extract_data.py
@@ -8,6 +8,32 @@ triggers = ["Trigger", "Entity"]
 # TODO: need to split on sentences, cant preprocess because the start and end position then need to be recalculated
 # TODO: () creating an issue in token match, either inexact or without() should be matched
 
+def CollapseMarkers(tagged_sentences):
+    # tagged_sentences format = [(sent_markers, trigger_markers, argument_markers, triggers_helpers)..]
+    tagger = dict()
+    for tagging in tagged_sentences:
+        if tuple(tagging[1]) not in tagger:
+            tagger[tuple(tagging[1])]=[]
+        tagger[tuple(tagging[1])].append(tuple(tagging[2]))
+        #print len(tagging[2])
+    return_markers = []
+    for t in tagger:
+        final_arg_markers = list(tagger[t][0]) # collapses each one into a single marking for each common trigger Arg1
+        #print final_arg_markers
+        #print len(tagger[t]), 'length'
+        #print tagger[t]
+        if len(tagger[t])>1:
+            for y in tagger[t]:
+                for marker in range(0, len(y)):
+                    if y[marker]!=final_arg_markers[marker] and y[marker]!='O':
+                        final_arg_markers[marker]=y[marker]
+        #print tagged_sentences[0][0]
+        return_markers.append((tagged_sentences[0][0], list(t), final_arg_markers))
+        #print final_arg_markers
+        #print '='*32
+    #print len(tagged_sentences)
+    return return_markers
+
 
 def ExtractTriggers(sentence, annotations, trigger_tokens):
     triggers, arguments = [], []
@@ -16,48 +42,56 @@ def ExtractTriggers(sentence, annotations, trigger_tokens):
     type_role = annotations[1] # semantic role
     arg1, arg2  = annotations[2].split(':')[1], annotations[3].split(':')[1]
     cur_start = 0
-    for token in sentence:
-        token_temp = token.replace('.','').replace(',','').replace('\(','').replace('\)','')
+    for token in sentence: # every word in the sentence
+        token_temp = token.replace('.','').replace(',','').replace('(','').replace(')','')
+        #print token_temp, token
+        found_trigger  = False
         if (token_temp, arg1) in trigger_tokens:
             start = trigger_tokens[(token_temp, arg1)][0]
             end =  trigger_tokens[(token_temp, arg1)][1]
             #print start, end, cur_start
             if cur_start>=int(start) and cur_start<=int(end):
                 triggers.append(1)
-                triggers_helpers.append(1)
+                found_trigger  = True
+                #triggers_helpers.append(0)
                 #print token
-        elif (token_temp, arg2) in trigger_tokens:
+        if not found_trigger:
+            triggers.append(0)
+        found_arg = False
+        if (token_temp, arg2) in trigger_tokens:
             start = trigger_tokens[(token_temp, arg2)][0]
             end =  trigger_tokens[(token_temp, arg2)][1]
-            #print start, end, cur_start
+            #print arg1, arg2, start, end, cur_start
             if cur_start>=int(start) and cur_start<=int(end):
-                triggers.append(1)
+                #triggers.append(0)
                 triggers_helpers.append(2)
                 #print token
-        else:
-            triggers.append(0)
+                found_arg = True
+        if not found_arg:
+            #triggers.append(0)
             triggers_helpers.append(0)
-        cur_start+=len(token)+1 # current start position of the word
 
+        cur_start+=len(token)+1 # current start position of the word
+    #print arg1, arg2
+    #print triggers_helpers
     return [s.replace('.','').replace(',','') for s in sentence], triggers, triggers_helpers
 
 def ExtractRoles(triggers_helpers, annotations):
     argument_markers = []
     #print annotations, triggers_helpers
-
     for a in range(0,len(triggers_helpers)):
         if triggers_helpers[a]==0:
             argument_markers.append('O')
-        if triggers_helpers[a]==1:
+        """if triggers_helpers[a]==1:
             if a!=0 and triggers_helpers[a-1]==1:
                 argument_markers.append('1-I-'+annotations[1])
             else:
-                argument_markers.append('1-B-'+annotations[1])
+                argument_markers.append('1-B-'+annotations[1])"""
         if triggers_helpers[a]==2:
             if a!=0 and triggers_helpers[a-1]==2:
-                argument_markers.append('2-I-'+annotations[1])
+                argument_markers.append('I-'+annotations[1])
             else:
-                argument_markers.append('2-B-'+annotations[1])
+                argument_markers.append('B-'+annotations[1])
     #print argument_markers
     return argument_markers
 
@@ -77,11 +111,14 @@ def Extract(sent_path, ann_path):
         if ann.split()[1] in semantic_roles:
             sent_markers, trigger_markers, triggers_helpers = ExtractTriggers(sent, ann.split(), trigger_tokens)
             argument_markers = ExtractRoles(triggers_helpers, ann.split())
-            temp_return.append((sent_markers, trigger_markers, argument_markers))
+            temp_return.append((sent_markers, trigger_markers, argument_markers, triggers_helpers))
             # TODO: fix here, it should save for each
     return temp_return
 
-#(sent_markers, trigger_markers, argument_markers) = Extract(sent_path, ann_path)
+#tagged_sentences =  Extract(sent_path, ann_path)  # format  = [(sent_markers, trigger_markers, argument_markers, triggers_helpers)..]
+#collapsed_sentences = CollapseMarkers(tagged_sentences)
+
+# TODO: Collapse into same trigger ones
 
 
 import os
@@ -96,7 +133,8 @@ for d in dir:
         if 'p'+str(i)+".txt" in files:
             file_txt = path+'p'+str(i)+".txt"
             file_ann = path+'p'+str(i)+".ann"
-            temp_return = Extract(file_txt, file_ann)
+            tagged_sentences = Extract(file_txt, file_ann)
+            temp_return = CollapseMarkers(tagged_sentences)
             if d=="test/": test+=temp_return
             else: train+=temp_return
             print file_txt
